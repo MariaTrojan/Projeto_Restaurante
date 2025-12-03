099d08e10e0437846cea908e37ff0665e11fa92d
MariaTrojan <mariaelviratrojan19@gmail.com>
2025-12-02 23:28:55 -0300

    Delete Projeto_restaurante directory

--- Projeto_restaurante/model/StatusPedido.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class StatusPedido(Enum):
-    EM_ABERTO = "Em aberto"
-    EM_PREPARO = "Em preparo"
-    PRONTO = "Pronto"
-    ENTREGUE = "Entregue"
-    CANCELADO = "Cancelado"
